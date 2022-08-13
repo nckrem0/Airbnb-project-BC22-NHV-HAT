7fa7ca5398e9433e96a5dba791bc8edc32dea04d
nckrem0 <nckrem0@gmail.com>
2022-08-13 10:02:55 +0700

    etiu upload code

--- src/pages/Ekui/Ekui.tsx
+++ src/pages/Ekui/Ekui.tsx
@@ -1,12 +1,12 @@
 type Props = {};
 
 const Ekui = (props: Props) => {
-  return (
-    <div>
-      Ekui
-      <h1>e tiu 2</h1>
-    </div>
-  );
+    return (
+        <div>
+            Ekui
+            <h1>e tiu 2</h1>
+        </div>
+    );
 };
 
 export default Ekui;
